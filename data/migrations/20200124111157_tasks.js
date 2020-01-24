f21046bdf1f74c484a2e1635d2a74b09600068e6
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-24 11:26:39 -0500

    created tasks table

--- data/migrations/20200124111157_tasks.js
+++ data/migrations/20200124111157_tasks.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('tasks', tbl => {
+        tbl.increments()
+        tbl.string('description', 128).notNullable
+        tbl.string('notes', 500)
+        tbl.boolean('completed').defaultTo(false)
+        tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('tasks')
   
 };
